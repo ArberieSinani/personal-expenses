9a4852e91475135d601f29271dae61e2c3959e4e
ArberieSinani <arberiesinani.07@gmail.com>
2025-07-15 20:50:40 +0200

    Project Init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+
+  return (
+
+      <div className='bg-gray-300 pt-50 pb-50 text-white'>
+        <Button variant="secondary">Testing Shadcn Button Component</Button>
+  </div>
+
+  )
+}
+
+export default App
